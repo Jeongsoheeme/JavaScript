500dac7d7dbc9477b7acf4de40f272d3515b9889
Jeongsohee <toy3045@gmail.com>
2023-01-12 21:45:42 +0900

    object

---
+++ 7.object.js
@@ -0,0 +1,48 @@
+let name = 'apple';
+let color = 'red';
+let display = '🍎';
+let orangeName = 'orange';
+
+let apple = {
+  name: 'apple',
+  color: 'red',
+  display: '🍎',
+};
+
+console.log(apple);
+console.log(apple.name);
+console.log(apple.color);
+console.log(apple.display);
+
+let orange = {
+  name: 'orange',
+  color: 'orange',
+  display: '🍊',
+};
+
+console.log(orange);
+console.log(orange.name);
+console.log(orange.color);
+console.log(orange.display);
+
+let lemon = {
+  name: 'lemon',
+  color: 'yellow',
+  display: '🍋',
+};
+
+console.log(lemon);
+console.log(lemon.name);
+console.log(lemon.color);
+console.log(lemon.display);
+
+let grape = {
+  name: 'grape',
+  color: 'purple',
+  display: '🍇',
+};
+
+console.log(grape);
+console.log(grape.name);
+console.log(grape.color);
+console.log(grape.display);
