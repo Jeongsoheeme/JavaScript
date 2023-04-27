226d595904cb37d7f9cb6502b0ba73e0cbf66027
Jeongsohee <toy3045@gmail.com>
2023-04-27 20:30:17 +0900

    array-method, shallow

--- 3.array-method.js
+++ 3.array-method.js
@@ -37,3 +37,50 @@ console.log(fruits); // 배열 자체를 수정(업데이트)
 console.log(deleted);
 fruits.splice(1, 0, '🍎', '🍇'); // 0이라서 중간에 안지우고 요소를 넣을 수 있다.
 console.log(fruits); // 배열 자체를 수정(업데이트)
+
+// 잘라진 새로운 배열을 만듬
+let newArr = fruits.slice(0, 2);
+console.log(newArr);
+console.log(fruits);
+let newArr2 = fruits.slice(-1);
+console.log(newArr2);
+
+// 여러개의 배열을 붙여줌
+const arr1 = [1,2,3];
+const arr2 = [4,5,6];
+const arr3 = arr1.concat(arr2);
+console.log(arr1);
+console.log(arr2);
+console.log(arr3);
+
+console.clear();
+
+// 순서를 거꾸로
+const arr4 = arr3.reverse();
+console.log(arr4);
+
+// 중첩 배열을 하나의 배열로 쫙 펴기
+let arr = [
+    [1,2,3],
+    [4,[5,6]],
+];
+console.log(arr);
+console.log(arr.flat(2)); //기존의 배열을 수정하지 않는다
+arr = arr.flat(2);
+
+// 특정한 값으로 배열을 채우기
+arr.fill(0);
+console.log(arr);
+arr.fill('s', 1, 3);
+console.log(arr);
+arr.fill('s', 1);
+console.log(arr); //배열 자체를 수정
+
+// 배열을 문자열로 합하기
+let text = arr.join();
+console.log(text);
+let text2 = arr.join(' | ');
+console.log(text2);
+
+
+
---
+++ 4.shallow.js
@@ -0,0 +1,14 @@
+// 얕은 복사 Shallow Copy - 객체는 메모리 주소 전달
+// 자바스크립트에서 복사할때는 항상 얕은 복사가 이루어짐!
+// Array.from, concat, slice, spread(...), object.assign
+const pizza = { name: '🍕', price: 4, owner: {name: 'Ellie'}};
+const ramen = { name: '🍜', price: 3};
+const sushi = { name: '🍣', price: 1};
+const store1 = [pizza, ramen];
+const store2 = Array.from(store1);
+console.log(store1);
+console.log(store2);
+
+store2.push(sushi);
+console.log(store1);
+console.log(store2);
